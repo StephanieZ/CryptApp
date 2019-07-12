a916dd096bba9dbd2f3f4792ae97e7f405116cba
Stephanie <stephanie.zinn@quest.com>
2019-07-12 10:38:26 -0700

    final checkin

--- DesCryptTest/Program.cs
+++ DesCryptTest/Program.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using CryptSharp;
 
 namespace CryptApp
 {
@@ -8,26 +12,40 @@ namespace CryptApp
   {
     static DesCrypt cCrypt = new DesCrypt();
     const string salt = "..";
-    private const int maxLength = 4;
+    public static int maxLength = 4;
+    public static int count = 0;
+    private static object jsLock = new object(); 
+
 
     static void Main(string[] args)
     {
-      
+
+      // maximum security AIX required a minimum of 4 chars in passwords, DES limits total password length to 8 chars
+
+      // for all 4 char strings
       CompareAllPasswords("", 0);
-      Debugger.Break();
+
+      // for all 5 char strings
+      maxLength = 5;
+      CompareAllPasswords("", 0);
+
+      // for all 6 char strings
+      maxLength = 6;
+      CompareAllPasswords("", 0);
+
+      // for all 7 char strings
+      maxLength = 7;
+      CompareAllPasswords("", 0);
+
+      // for all 8 char strings
+      maxLength = 8;
+      CompareAllPasswords("", 0);
+
     }
   
 
 
-// maximum security AIX required a minimum of 4 chars in passwords, DES limits total password length to 8 chars
 
-// for all 4 char strings
-
-
-// for all 5 char strings
-// for all 6 char strings
-// for all 7 char strings
-// for all 8 char strings
 
 
 
@@ -35,50 +53,84 @@ namespace CryptApp
 
     public static void CompareAllPasswords(string password, int length)
     {
-    
         length += 1;
-        foreach (var c in DesCrypt.Ascii64)
+
+      foreach (var c in DesCrypt.Ascii64)
+      {
+        count++;
+        if (length == maxLength &&
+            length == (password + c).Length &&
+            count >= Math.Pow(10, maxLength))
         {
-          if(length == maxLength && length == (password+c).Length)
-            ComparePassword(password+c);
-          if (length < maxLength)
+          try
           {
-            CompareAllPasswords(password + c, length);
+            count = 0;
+            Task.Run(() => ComparePassword(password + c));
           }
+          catch
+          {
+            //if we fail to start some passwords, no biggie, just skip them until memory is availble again.
+          }
+
         }
-      
+
+        if (length < maxLength)
+        {
+          CompareAllPasswords(password + c, length);
+        }
+      }
     }
 
     private static void ComparePassword(string password)
     {
-      var cCryptString = cCrypt.Descrypt(password, salt);
+      
+      string outputFile = "passwordComparison.log";
 
-      var jCompareProcess = new Process
+      //as it turns out, the standard out isn't threadsafe.
+      lock (jsLock)
       {
-        StartInfo = new ProcessStartInfo
+        var cCryptString = cCrypt.Descrypt(password, salt);
+
+        var oldCheckPassword = Crypter.CheckPassword(password, cCryptString);
+
+        var jCompareProcess = new Process
         {
-          FileName = "node",
-          Arguments = $"..\\..\\app.js {password} {salt}",
-          UseShellExecute = false,
-          RedirectStandardOutput = true,
-          CreateNoWindow = true
+          StartInfo = new ProcessStartInfo
+          {
+            FileName = "node",
+            Arguments = $"..\\..\\app.js {password} {salt}",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+          }
+        };
+
+        jCompareProcess?.Start();
+
+        jCompareProcess?.WaitForExit();
+
+        var jCryptString = jCompareProcess.StandardOutput.ReadLine();
+
+
+        File.AppendAllText(outputFile, password + '\n');
+        File.AppendAllText(outputFile, jCryptString + '\n');
+        File.AppendAllText(outputFile, cCryptString + '\n');
+        if (string.CompareOrdinal(jCryptString, cCryptString) == 0)
+        {
+          File.AppendAllText(outputFile, "A password check succeeded\n");
+          if (!oldCheckPassword)
+          {
+            File.AppendAllText(outputFile, "The only algorithm would have failed");
+          }
         }
-      };
+        else
+        {
+          File.AppendAllText(outputFile, "A password check failed\n");
+        }
 
-      jCompareProcess?.Start();
 
-      jCompareProcess?.WaitForExit();
-
-      var jCryptString = jCompareProcess.StandardOutput.ReadLine();
-
-      if (string.CompareOrdinal(jCryptString, cCryptString) != 0)
-      {
-        Console.WriteLine(password);
-        Console.WriteLine(jCryptString);
-        Console.WriteLine(cCryptString);
-        Console.WriteLine("A password check failed");
-        Debugger.Break();
       }
+
     }
   }
 }
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+C# DES Encryption and Decryption
+
+Sometimes developers have a need to verify passwords from Unix-based systems on Windows-based applications. This presents an issue, in that .NET crypto libraries like CryptSharp© do not support a subset of the salts that Unix does. They flat out fail to encrypt or they throw an invalid salt exception. So this library is intended to expand the existing .NET crypto libraries so they all the salts that are acceptable to Unix are also acceptable to your .NET Windows application
